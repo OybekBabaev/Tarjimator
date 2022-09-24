5a3aae6c3bffc134ad392e1ad68e2789fe8c7812
Oybek Babaev <oybekbabaev15@yahoo.com>
2022-09-24 23:14:50 +0500

    Add fallback route message

--- Program.cs
+++ Program.cs
@@ -7,6 +7,9 @@ builder.Services.AddSingleton<TarjimationEngine>();
 
 var app = builder.Build();
 
+app.UseRouting();
 app.MapRazorPages();
+app.MapFallback(async ctx => await ctx.Response
+        .WriteAsync("Oops! Seems like you\'ve just requested a non-existent page."));
 
 app.Run();
